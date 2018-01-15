d244e2f90f089277d59793489d22bd5ee58ec9c0
Richard Loera <richexp9@gmx.com>
2018-01-15 00:41:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,10 @@ end
 
 def summon_captain_planet(array)
   array.each_with_index { |e, i|
-    }
+    string.slice(0,1).upcase + string[1,string.size]
+  }
   array
-
+  string.slice(0,1).upcase + string[1,string.size]
 end
 
 def long_planeteer_calls(array)
