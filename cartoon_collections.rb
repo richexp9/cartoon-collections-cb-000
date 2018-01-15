ab7320495a2c74fa1402ab98bf3b9f465aacbac8
Richard Loera <richexp9@gmx.com>
2018-01-15 00:32:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,11 @@
 def roll_call_dwarves(array)
-end
-
-def summon_captain_planet(array)
   array.each_with_index { |e, i|
     puts "#{i}. #{e}"
   }
+end
+
+def summon_captain_planet(array)
+
 end
 
 def long_planeteer_calls(array)
