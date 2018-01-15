ee8c3e4f85593de3f127a9959398d1633ee0dff9
Richard Loera <richexp9@gmx.com>
2018-01-15 00:30:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
-def roll_call_dwarves
+def roll_call_dwarves(array)
 end
 
-def summon_captain_planet
+def summon_captain_planet(array)
 end
 
-def long_planeteer_calls
+def long_planeteer_calls(array)
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
